f5fe88d7fda4e7f21db6c5bf640827dfdbad1fc7
Eric Zhao <90228788+ericezzhao@users.noreply.github.com>
2025-03-14 12:12:55 -0700

    Update load_llava_model.py

--- model/load_llava_model.py
+++ model/load_llava_model.py
@@ -1,6 +1,6 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_name = "scratch/$USER/scale-equity-nlp/model"
+model_name = "/scratch/$USER/models/llava-v1.5-7b"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
