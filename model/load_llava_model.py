c9faa246a5b49f56efd3316330c9f819f5e6f0fc
Eric Zhao <90228788+ericezzhao@users.noreply.github.com>
2025-03-14 11:50:53 -0700

    Update load_llava_model.py

--- model/load_llava_model.py
+++ model/load_llava_model.py
@@ -1,6 +1,6 @@
 from transformers import AutoModelForCasualLM, AutoTokenizer
 
-model_name = "path/to/llava_model" # update with actual path
+model_name = "scratch/$USER/models/llava-v1.5-7b"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
