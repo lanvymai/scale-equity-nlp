9461057509cbf33472e2ef1e48d40459cb240ffd
Eric Zhao <90228788+ericezzhao@users.noreply.github.com>
2025-03-14 12:35:24 -0700

    Update load_llava_model.py

--- model/load_llava_model.py
+++ model/load_llava_model.py
@@ -1,8 +1,9 @@
-model_name = "/scratch/ez2545/models/llava-v1.5-7b"
+from transformers import AutoTokenizer
+from llava.model import LlavaForConditionalGeneration
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+model_path = "/scratch/ez2545/models/llava-v1.5-7b"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+model = LlavaForConditionalGeneration.from_pretrained(model_path, torch_dtype="auto", device_map="auto")
 
 print("Model Loaded Successfully")
