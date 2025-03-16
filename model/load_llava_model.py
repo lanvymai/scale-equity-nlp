bf8c62b6bb8b59cd06ac01f82274da3f3f9275de
Eric Zhao <90228788+ericezzhao@users.noreply.github.com>
2025-03-15 21:24:09 -0700

    Update load_llava_model.py

--- model/load_llava_model.py
+++ model/load_llava_model.py
@@ -1,9 +1,8 @@
-import os
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_path = f"/scratch/{os.environ['USER']}/models/llava-v1.5-7b"
+model_name = "/scratch/ez2545/models/llava-v1.5-7b"
 
-tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype="auto", device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, use_fast=False)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto", trust_remote_code=True)
 
 print("Model Loaded Successfully")
