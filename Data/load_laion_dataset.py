edb66877ac1c1758cda89f5eccfa5c408df26d66
Eric Zhao <90228788+ericezzhao@users.noreply.github.com>
2025-03-16 12:12:02 -0700

    Update load_laion_dataset.py

--- Data/load_laion_dataset.py
+++ Data/load_laion_dataset.py
@@ -1,6 +1,5 @@
 from datasets import load_dataset
 
-dataset = load_dataset("laion/laion400m", split='train')
-dataset.save_to_disk("/scratch/ez2545/datasets/laion400m")
+ds = load_dataset("laion/laion400m")
 
 print("Dataset Downloaded Successfully!")
