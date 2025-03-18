8aee17e45c63728e5343639b502011a7423879c9
Eric Zhao <90228788+ericezzhao@users.noreply.github.com>
2025-03-18 13:02:19 -0700

    Update load_llava_model.py

--- model/load_llava_model.py
+++ model/load_llava_model.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForVision2Seq
 
-model_path = "/scratch/eyw2010/models/llava-v1.5-7b"
+model_path = "/scratch/eyw2010"
 
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True, use_fast=False)
 model = AutoModelForVision2Seq.from_pretrained(model_path, torch_dtype="auto", device_map="auto", trust_remote_code=True)
