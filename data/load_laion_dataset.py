9fba8e17ecde7a07f9c2ca3f757279b6b8e7985e
Eric Zhao <90228788+ericezzhao@users.noreply.github.com>
2025-03-23 12:42:31 -0700

    Update load_laion_dataset.py

--- data/load_laion_dataset.py
+++ data/load_laion_dataset.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 
-ds = load_dataset("laion/laion400m", split="train[25%:50%]")
+ds = load_dataset("laion/laion400m", split="train[50%:75%]")
 
-ds.to_parquet("/scratch/ez2545/scale-equity-nlp/Data/laion400m-2.parquet")
+ds.to_parquet("/scratch/ez2545/scale-equity-nlp/Data/laion400m-3.parquet")
 
 print("Dataset Saved as Parquet!")
